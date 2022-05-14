6a76faf89d23f7f31c154e302f22441e9c5de95c
Yana Hvozdetska <yhvozdetska@gmail.com>
2022-05-14 20:06:01 +0200

    Convert List to LinkedHashMap
    Signed-off-by: Yana Hvozdetska <yhvozdetska@gmail.com>

---
+++ src/E8.java
@@ -0,0 +1,16 @@
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class E8 {
+    public static void main(String[] args) {
+        System.out.println(convertListToLinkedHashMap(List.of("Bluehost", "GoDaddy", "Amazon AWS", "LiquidWeb", "FatCow")));
+    }
+
+    public static Map<String, Integer> convertListToLinkedHashMap(final List<String> strings) {
+        return strings.stream()
+                .collect(Collectors.toMap(Function.identity(),String::length));
+    }
+}
