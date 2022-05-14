8d1b8803f386137fcd0ef7bd03674fe9a16ead95
Yana Hvozdetska <yhvozdetska@gmail.com>
2022-05-14 12:00:57 +0200

    first version with factorial
    Signed-off-by: Yana Hvozdetska <yhvozdetska@gmail.com>

--- src/E1.java
+++ src/E1.java
@@ -1,10 +1,9 @@
 public class E1 {
     static int factorialCounter(int x) {
-        int result = 1;
-        for (int i = 1; i <=x; i ++){
-            result = result*i;
-        }
-        return result;
+        if (x == 1)
+            return 1;
+        else
+            return x * factorialCounter(x - 1);
     }
 
     public static void main(String[] args) {
