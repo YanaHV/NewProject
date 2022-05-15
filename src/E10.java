11867a2348d2466651995ce1ebe21b5d6753f583
Yana Hvozdetska <yhvozdetska@gmail.com>
2022-05-15 15:21:22 +0200

    convert to Map with Exception
    Signed-off-by: Yana Hvozdetska <yhvozdetska@gmail.com>

---
+++ src/E10.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class E10 {
+    public static void main(String[] args) {
+        System.out.println(convertListToTreeMapWithException(List.of("Bluehost", "GoDaddy", "Bluehost")));
+    }
+    public static Map<Integer, String> convertListToTreeMapWithException(final List<String> strings) throws IllegalArgumentException{
+        return strings.stream()
+                .collect(Collectors.toMap(Function.identity(),
+                        String::length,
+                        (a,b) -> new IllegalArgumentException("Помилка!"),
+                        TreeMap::new));
+    }
+}
