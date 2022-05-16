1b2df76d9142b9cd549524b6c1fdf205b1df783f
Yana Hvozdetska <yhvozdetska@gmail.com>
2022-05-16 18:27:04 +0200

    first version with factorial 1
    Signed-off-by: Yana Hvozdetska <yhvozdetska@gmail.com>

--- src/E2.java
+++ src/E2.java
@@ -1,14 +1,12 @@
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class E2 {
 
     public static Map<Integer, List<String>> groupByStringLength(final List<String> stringList) {
-        final Map<Integer, List<String>> map = stringList.parallelStream()
+        return stringList.stream()
                 .collect(Collectors.groupingBy(String::length));
-        return map;
     }
 
     public static void main(String[] args) {
